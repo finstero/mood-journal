bcc37104869edbb865ccfcd3ea281fcb46faa7be
Olivia Finster <olivia.finster@gmail.com>
2023-06-13 20:29:36 -0500

    entry cards

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,8 @@
+import EntryCard from "@/components/EntryCard";
+import NewEntryCard from "@/components/NewEntryCard";
 import { getUserByClerkID } from "@/utils/auth";
 import { prisma } from "@/utils/db";
+import Link from "next/link";
 
 const getEntries = async () => {
   const user = await getUserByClerkID();
@@ -20,8 +23,16 @@ const entries = await getEntries();
 console.log('entries', entries);
 
   return (
-    <div>
-      <div>journal</div>
+    <div className="p-10 bg-zinc-400/10 h-full">
+      <h2 className="text-3xl mb-8">Journal</h2>
+      <div className="grid grid-cols-3 gap-4 ">
+        <NewEntryCard />
+        {entries.map((entry) => (
+          <Link href={`/journal/${entry.id}`} key={entry.id}>
+            <EntryCard entry={entry} />
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
---
+++ components/EntryCard.tsx
@@ -0,0 +1,18 @@
+import { JournalEntry } from "@prisma/client";
+
+interface EntryCardProps {
+  entry: JournalEntry;
+}
+
+const EntryCard = ({entry}: EntryCardProps) => {
+  const date = new Date(entry.createdAt).toDateString()
+  return (
+    <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+      <div className="px-4 py-5">{date}</div>
+      <div className="px-4 py-5">summary</div>
+      <div className="px-4 py-4">mood</div>
+    </div>
+  )
+}
+
+export default EntryCard;
