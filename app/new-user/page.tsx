f4d877eaa506abdeb09c1abdc48683550d3a87cf
Olivia Finster <olivia.finster@gmail.com>
2023-06-13 19:45:03 -0500

    create clerk user in mysql database

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,8 +1,33 @@
+import { prisma } from '@/utils/db'
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
 
-const NewUser = () => {
-  return(
-    <div>HELLO</div>
-  )
+const createNewUser = async () => {
+  const user = await currentUser()
+
+  console.log('user', user)
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id as string,
+    },
+  })
+
+  if (!match) {
+    const newUser = await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    })
+  }
+
+  redirect('/journal')
 }
 
-export default NewUser;
+const NewUser = async () => {
+  await createNewUser()
+  return <div>HELLO</div>
+}
+
+export default NewUser
